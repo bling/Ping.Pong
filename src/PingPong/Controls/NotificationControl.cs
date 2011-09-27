e112bad91c9124d7c16aeef3d62b1f5994132dee
bling <bling@live.ca>
2011-09-26 23:03:56 -0400

    add notification control

---
+++ src/PingPong/Controls/NotificationControl.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace PingPong.Controls
+{
+    public class NotificationControl : ContentControl
+    {
+        public NotificationControl()
+        {
+            DefaultStyleKey = typeof(NotificationControl);
+        }
+    }
+}
