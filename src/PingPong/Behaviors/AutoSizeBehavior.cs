c0b16344a9998526586bff9ff659a5fac7e261af
bling <bling@live.ca>
2011-09-28 10:27:01 -0400

    allow autosizing to work without attached property

--- src/PingPong/Behaviors/AutoSizeBehavior.cs
+++ src/PingPong/Behaviors/AutoSizeBehavior.cs
@@ -1,46 +1,54 @@
-﻿using System;
-using System.Collections.Specialized;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Interactivity;
-
-namespace PingPong.Behaviors
+﻿namespace PingPong.Behaviors
 {
+    using System;
+    using System.Collections.Specialized;
+    using System.Windows;
+    using System.Windows.Controls;
+    using System.Windows.Interactivity;
+    using System.Windows.Media;
+    using PingPong.Core;
+
     public class AutoSizeBehavior : Behavior<ItemsControl>
     {
-        public static readonly DependencyProperty ItemsControlProperty =
-            DependencyProperty.Register("ItemsControl",
-                                        typeof(ItemsControl),
-                                        typeof(AutoSizeBehavior),
-                                        new PropertyMetadata(OnItemsSourceChanged));
-
-        public ItemsControl ItemsControl
-        {
-            get { return (ItemsControl)GetValue(ItemsControlProperty); }
-            set { SetValue(ItemsControlProperty, value); }
-        }
+        private ItemsControl _itemsControl;
 
         protected override void OnAttached()
         {
             base.OnAttached();
-            OnItemsControlSizeChanged(null, null);
+            AssociatedObject.Loaded += delegate
+            {
+                ItemsControl ic;
+                DependencyObject current = AssociatedObject;
+                DependencyObject parent;
+                do
+                {
+                    parent = VisualTreeHelper.GetParent(current);
+                    ic = parent as ItemsControl;
+                    current = parent;
+                } while (parent != null && ic == null);
+
+                AttachTo(ic);
+                OnItemsControlSizeChanged(null, null);
+            };
         }
 
-        private static void OnItemsSourceChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        private void AttachTo(ItemsControl itemsControl)
         {
-            var behavior = (AutoSizeBehavior)d;
-            behavior.ItemsControl.SizeChanged -= behavior.OnItemsControlSizeChanged;
-            behavior.ItemsControl.SizeChanged += behavior.OnItemsControlSizeChanged;
+            Enforce.NotNull(itemsControl);
 
-            var collection = behavior.ItemsControl.ItemsSource as INotifyCollectionChanged;
+            _itemsControl = itemsControl;
+            _itemsControl.SizeChanged -= OnItemsControlSizeChanged;
+            _itemsControl.SizeChanged += OnItemsControlSizeChanged;
+
+            var collection = _itemsControl.ItemsSource as INotifyCollectionChanged;
             if (collection != null)
-                collection.CollectionChanged += behavior.OnItemsControlSizeChanged;
+                collection.CollectionChanged += OnItemsControlSizeChanged;
         }
 
         private void OnItemsControlSizeChanged(object sender, EventArgs e)
         {
-            if (ItemsControl.Items.Count > 0)
-                AssociatedObject.Width = ItemsControl.ActualWidth / ItemsControl.Items.Count;
+            if (_itemsControl.Items.Count > 0)
+                this.AssociatedObject.Width = _itemsControl.ActualWidth / _itemsControl.Items.Count;
         }
     }
 }
