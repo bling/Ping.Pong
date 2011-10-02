9c767f77d1461d3c990d3835af1fb8d0422798b9
bling <bling@live.ca>
2011-10-02 14:45:53 -0400

    dispose subscription on unload style authorization view a little bit rename file to match class name

--- src/PingPong/Behaviors/RefreshDateBehavior.cs
+++ src/PingPong/Behaviors/RefreshDateBehavior.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Linq;
 using System.Reactive.Linq;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Interactivity;
 using PingPong.Models;
@@ -14,12 +15,19 @@ namespace PingPong.Behaviors
         protected override void OnAttached()
         {
             base.OnAttached();
+            AssociatedObject.Unloaded += OnUnloaded;
             _subscription = Observable.Interval(TimeSpan.FromSeconds(20))
                 .DispatcherSubscribe(_ =>
                 {
                     foreach (var tweet in AssociatedObject.Items.OfType<ITweetItem>())
                         tweet.NotifyOfPropertyChange("CreatedAt");
                 });
+        }
+
+        private void OnUnloaded(object sender, RoutedEventArgs e)
+        {
+            AssociatedObject.Unloaded -= OnUnloaded;
+            _subscription.Dispose();
         }
 
         protected override void OnDetaching()
